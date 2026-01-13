58fff6656336ff6e329550363f79264397823765
felipesora <felipeusora@gmail.com>
2026-01-13 15:05:57 -0300

    init: começando seção de experiencia e educação

---
+++ src/components/Experiencia/index.tsx
@@ -0,0 +1,14 @@
+const Experiencia = () => {
+    return (
+        <section className="bg-[#EBEBEB] pt-24 pb-32 px-40 flex flex-col gap-14">
+
+            <div className="flex flex-col gap-4">
+                <h2 className="text-4xl font-bold">Experiência & Educação</h2>
+                <p className="text-base text-[#6C757D]">Minha jornada profissional e acadêmica até o momento</p>
+            </div>
+
+        </section>
+    );
+};
+
+export default Experiencia;
