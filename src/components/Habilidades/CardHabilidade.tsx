391382be6cfda842e73c33e684376dc734310b56
felipesora <felipeusora@gmail.com>
2026-01-14 16:45:42 -0300

    wip: card de habilidades

--- src/components/Habilidades/CardHabilidade.tsx
+++ src/components/Habilidades/CardHabilidade.tsx
@@ -1,7 +1,10 @@
-const CardHabilidade = () => {
-    return (
-        <div>
+import type { CardHabilidadeProps } from "./types";
 
+const CardHabilidade = ({ nome, icone }: CardHabilidadeProps) => {
+    return (
+        <div className="bg-white border border-[#DEE2E6] rounded-[10px] flex flex-col gap-1 items-center justify-center py-5 w-37.5 h-32">
+            <img src={icone} alt={`ícone de ${nome}`} />
+            <h5 className="font-semibold wrap-break-word text-center">{nome}</h5>
         </div>
     )
 }
--- src/components/Habilidades/index.tsx
+++ src/components/Habilidades/index.tsx
@@ -1,3 +1,5 @@
+import CardCategoriaHabilidade from "./CardCategoriaHabilidade";
+
 const Habilidades = () => {
     return (
         <section className="bg-white pt-24 pb-32 px-40 flex flex-col gap-14">
@@ -5,6 +7,14 @@ const Habilidades = () => {
             <div className="flex flex-col gap-4">
                 <h2 className="text-4xl font-bold">Minhas Habilidades</h2>
                 <p className="text-base text-[#6C757D]">Tecnologias e ferramentas que utilizo para criar soluções eficientes</p>
+            </div>
+
+            {/* Definir o width full, mas manter um min width, assim usando space between */}
+            {/* Ver de usar um wrap, para quebrar e colocar 2 em cima e 1 em baixo centralizado */}
+            <div className="flex flex-col gap-52">
+                <CardCategoriaHabilidade categoria="front" />
+                <CardCategoriaHabilidade categoria="back" />
+                <CardCategoriaHabilidade categoria="outros" />
             </div>
 
         </section>
