d0ce43979da1b7cda0bf1da694fbd5a8b652142f
felipesora <felipeusora@gmail.com>
2026-01-20 15:20:38 -0300

    feat: seção de certificados

---
+++ src/components/Certificados/CardCertificado.tsx
@@ -0,0 +1,19 @@
+import type { CardCertificadoProps } from "./types";
+import IconeCertificado from "../../assets/icons/icone-certificado.png"
+
+const CardCertificado = ({ nome, instituicao, link }: CardCertificadoProps) => {
+    return (
+        <div className="bg-[#EBEBEB] shadow-sm rounded-[10px] py-16 px-24 flex flex-col gap-5 items-center border border-transparent hover:border-[#111111] hover:-translate-y-1 hover:shadow-md transition-all duration-300 ease-out">
+            <img src={IconeCertificado} alt="icone certificado" />
+
+            <div className="flex flex-col gap-1 items-center">
+                <h4 className="font-bold text-[#313131] text-xl">{nome}</h4>
+                <p className="text-[#6C757D]">{instituicao}</p>
+            </div>
+
+            <a href={link} className="text-center font-bold text-[#313131] border border-[#313131] py-3 px-8 rounded-[10px] transition-colors duration-300 ease-in-out hover:bg-[#313131] hover:text-white">Ver Certificado</a>
+        </div>
+    )
+}
+
+export default CardCertificado;
---
+++ src/components/Certificados/index.tsx
@@ -0,0 +1,28 @@
+import CardCertificado from "./CardCertificado";
+import { certificados } from "./dataCertificados";
+
+const Certificados = () => {
+    return (
+        <section className="bg-white pt-24 pb-32 px-40 flex flex-col gap-14">
+        
+            <div className="flex flex-col gap-4">
+                <h2 className="text-4xl font-bold">Certificados</h2>
+                <p className="text-base text-[#6C757D]">Certificações que adquiri ao longo da minha jornada de aprendizado</p>
+            </div>
+            
+            <div className="flex flex-wrap justify-between gap-y-10">
+                {certificados.map((certificado) => (
+                    <CardCertificado 
+                        key={certificado.id} 
+                        nome={certificado.nome} 
+                        instituicao={certificado.instituicao} 
+                        link={certificado.link}
+                    />
+                ))}
+            </div>
+
+        </section>
+    )
+}
+
+export default Certificados;
