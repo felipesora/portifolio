c76a50ccdab1507b39fe810cd5e2191210976fd1
felipesora <felipeusora@gmail.com>
2026-03-04 14:09:50 -0300

    feat: responsividade da tela

--- src/pages/ProjetoDetalhe/components/CardDetalhesProjeto.tsx
+++ src/pages/ProjetoDetalhe/components/CardDetalhesProjeto.tsx
@@ -16,41 +16,41 @@ const CardDetalhesProjeto = () => {
     }
 
     return (
-        <div className="bg-white w-[80%] rounded-[10px] shadow-sm">
+        <div className="bg-white w-[80%] rounded-[10px] shadow-sm [@media(max-width:1375px)]:w-full">
 
-            <img src={projeto.imagem} alt={`Imagem do projeto ${projeto.titulo}`} className="w-full h-125 object-cover object-top rounded-t-[10px]" />
+            <img src={projeto.imagem} alt={`Imagem do projeto ${projeto.titulo}`} className="w-full h-[200px] object-cover object-top rounded-t-[10px] border-b border-gray-300 2xl:h-125 xl:h-[450px] lg:h-[350px] md:h-[400px] sm:h-[250px]" />
 
-            <div className="flex flex-col gap-7 p-12">
+            <div className="flex flex-col gap-7 p-12 [@media(max-width:1000px)]:px-7">
                 <div className="flex flex-col gap-4">
                     <h1 className="text-4xl font-bold [@media(max-width:550px)]:text-2xl">{projeto.titulo}</h1>
-                    <p className="text-base text-[#6C757D] [@media(max-width:550px)]:text-sm">{projeto.descricao}</p>
+                    <p className="text-base text-justify text-[#6C757D] [@media(max-width:550px)]:text-sm">{projeto.descricao}</p>
                 </div>
 
-                <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-4 [@media(max-width:1023px)]:hidden">
                     <h2 className="font-bold text-2xl [@media(max-width:550px)]:text-xl">Galeria</h2>
-                    <div className="flex justify-between">
+                    <div className="grid grid-cols-2 gap-6">
                         {projeto.galeria?.map((imagem) => (
-                            <img src={imagem.imagem} alt="Imagem do projeto" className="w-112.5 rounded-[10px] transition-all duration-500 ease-in-out hover:scale-110 hover:shadow-xl" />
+                            <img src={imagem.imagem} alt="Imagem do projeto" className="w-full rounded-[10px] transition-all duration-500 ease-in-out hover:scale-110 hover:shadow-xl" />
                         ))}
                     </div>
                 </div>
 
                 <div className="flex flex-col gap-4">
                     <h2 className="font-bold text-2xl [@media(max-width:550px)]:text-xl">Tecnologias Utilizadas</h2>
-                    <div className="flex gap-3">
+                    <div className="flex flex-wrap gap-3">
                         {projeto.tecnologias.map((tecnologia) => (
                             <CardTecnologia nome={tecnologia.titulo} />
                         ))}
                     </div>
                 </div>
 
-                <div className="flex gap-4">
+                <div className="flex gap-4 [@media(max-width:850px)]:flex-col [@media(max-width:850px)]:gap-2">
                     <a href={projeto.linkCodigo} target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 text-white font-bold bg-[#313131] justify-center rounded-[10px] w-full py-3 cursor-pointer hover:bg-[#252525] transition-colors duration-300 [@media(max-width:550px)]:text-[14px] [@media(max-width:550px)]:py-2">
                         <img src={IconeGithub} alt="ícone github" className="[@media(max-width:550px)]:w-7"/>
                         Código Fonte
                     </a>
                     <a href={projeto.linkVideo} target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 text-white font-bold bg-[#FF0000] justify-center rounded-[10px] w-full py-3 cursor-pointer hover:bg-[#ad0606] transition-colors duration-300 [@media(max-width:550px)]:text-[14px]">
-                        <img src={IconeYoutube} alt="ícone do youtube" className="[@media(max-width:550px)]:w-7"/>
+                        <img src={IconeYoutube} alt="ícone do youtube" className="[@media(max-width:550px)]:w-6"/>
                         Ver Vídeo
                     </a>
                 </div>
--- src/pages/ProjetoDetalhe/index.tsx
+++ src/pages/ProjetoDetalhe/index.tsx
@@ -12,7 +12,7 @@ const ProjetoDetalhe = () => {
     return (
         <>
             <Cabecalho tipo="detalhesProjeto"/>
-            <section className="bg-[#EBEBEB] pt-48 pb-32 px-40 flex justify-center gap-14 [@media(max-width:1180px)]:px-20 [@media(max-width:690px)]:px-10 [@media(max-width:550px)]:px-5! [@media(max-width:550px)]:pt-16 [@media(max-width:550px)]:pb-20">
+            <section className="bg-[#EBEBEB] pt-44 pb-32 px-40 flex justify-center gap-14 [@media(max-width:1180px)]:px-20 [@media(max-width:1080px)]:pt-32 [@media(max-width:930px)]:px-10 [@media(max-width:570px)]:pt-24 [@media(max-width:550px)]:px-5! [@media(max-width:550px)]:pb-20">
                 <CardDetalhesProjeto />
             </section>
             <Rodape tipo="detalhesProjeto" />
